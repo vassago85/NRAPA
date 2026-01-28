ab23e234042b8fab975e880724bc8e5df7e2e0a2
vassago85 <p.charsley@gmail.com>
2026-01-28 07:59:34 +0200

    Disable SSL for mariadb driver to fix PHP 8.3 connection issues

--- config/database.php
+++ config/database.php
@@ -79,9 +79,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => extension_loaded('pdo_mysql') ? [
+                PDO::MYSQL_ATTR_SSL_CA => '',
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ] : [],
         ],
 
         'pgsql' => [
